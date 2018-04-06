4be04b3d5c6149dbfce26e6a63123efbcd13e056
liuaotiangithub <1151172004@qq.com>
2018-04-06 21:31:52 +0800

    改成函数形式，更改了一个接口，只需要传入SavaAndSet一个图片的URL即可

--- 壁纸.py
+++ 壁纸.py
@@ -6,57 +6,61 @@ import os
 import re
 import ctypes
 
-hand_url = "https://bing.ioliu.cn/"
-print("Look for image...")
-headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"}
-r = requests.get("https://bing.ioliu.cn/", headers=headers)  
-#使用headers以表示尊重
-soup = BeautifulSoup(r.text, "html.parser")
-count = randint(0, 11)
-div = soup.find_all("div", class_="item")[count]
-url = hand_url + div.div.a["href"]
-r = requests.get(url, headers=headers)
-search_data = re.compile(r'data-progressive="(.*?)"')
-image_url = search_data.findall(r.text)[0]
-image = requests.get(image_url, headers=headers)
-if system() == "Windows":
-	#if system is Windows
-	Path = "C://壁纸"
-	if os.path.exists(Path):
-		#if folder exist
-		with open("C://壁纸/1.jpg", "wb") as f:
-			f.write(image.content)
-			print("download image file over!")
-			#write file
-	else:
-		#if folder not exist
-		os.mkdir(Path)
-		with open("C://壁纸/1.jpg", "wb") as f:
-			f.write(image.content)
-			print("download image file over!")
-			#write file
-	ctypes.windll.user32.SystemParametersInfoW(20, 0, "C://壁纸/1.jpg", 0)
-	print("End of setting!")
-elif system() == "Linux":
-	#if system is Linux
-	Path = "/home/壁纸"
-	if os.path.exists(Path):
-		#if folder exist
-		with open("/home/壁纸/1.jpg", "wb") as f:
-			f.write(image.content)
-			print("download image file over!")
-			#write file
-	else:
-		#if folder not exist
-		os.mkdir(Path)
-		print("mkdir folder in ", os.getcwd())
-		with open("/home/壁纸/1.jpg", "wb") as f:
-			f.write(image.content)
-			print("download image file over!")
-			#write file
+def SaveAndSet(image_url):
+	image = requests.get(image_url, headers=headers)
+	if system() == "Windows":
+		#if system is Windows
+		Path = "C://壁纸"
+		if os.path.exists(Path):
+			#if folder exist
+			with open("C://壁纸/1.jpg", "wb") as f:
+				f.write(image.content)
+				print("download image file over!")
+				#write file
+		else:
+			#if folder not exist
+			os.mkdir(Path)
+			with open("C://壁纸/1.jpg", "wb") as f:
+				f.write(image.content)
+				print("download image file over!")
+				#write file
+		ctypes.windll.user32.SystemParametersInfoW(20, 0, "C://壁纸/1.jpg", 0)
+		print("End of setting!")
+	elif system() == "Linux":
+		#if system is Linux
+		Path = "/home/壁纸"
+		if os.path.exists(Path):
+			#if folder exist
+			with open("/home/壁纸/1.jpg", "wb") as f:
+				f.write(image.content)
+				print("download image file over!")
+				#write file
+		else:
+			#if folder not exist
+			os.mkdir(Path)
+			print("mkdir folder in ", os.getcwd())
+			with open("/home/壁纸/1.jpg", "wb") as f:
+				f.write(image.content)
+				print("download image file over!")
+				#write file
 
-	os.system('gsettings set org.gnome.desktop.background picture-uri "file:/home/壁纸/1.jpg"')
-	print("End of setting!")
-else:
-	#not know system
-	print("not know system!")
+		os.system('gsettings set org.gnome.desktop.background picture-uri "file:/home/壁纸/1.jpg"')
+		print("End of setting!")
+	else:
+		#not know system
+		print("not know system!")
+
+if __name__ == "__main__":
+	hand_url = "https://bing.ioliu.cn/"
+	print("Look for image...")
+	headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"}
+	r = requests.get("https://bing.ioliu.cn/", headers=headers)  
+	#使用headers以表示尊重
+	soup = BeautifulSoup(r.text, "html.parser")
+	count = randint(0, 11)
+	div = soup.find_all("div", class_="item")[count]
+	url = hand_url + div.div.a["href"]
+	r = requests.get(url, headers=headers)
+	search_data = re.compile(r'data-progressive="(.*?)"')
+	image_url = search_data.findall(r.text)[0]
+	SaveAndSet(image_url)
